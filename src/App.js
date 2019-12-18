48111d2bde4c9b30d7106b6c0e94c902b655d49a
MrMargaryan <hamlet.java@gmail.com>
2019-12-19 00:34:46 +0300

    installing dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+
     </div>
-  );
+  )
 }
 
 export default App;
